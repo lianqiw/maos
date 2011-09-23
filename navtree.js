34882d759a9a77609a2ee1cf4681493640d925bd
Lianqi Wang <lianqiw@gmail.com>
2011-09-23 10:45:23 -0700

    Updated documentation.

---
+++ navtree.js
@@ -0,0 +1,451 @@
+var NAVTREE =
+[
+  [ "maos-0.9.3", "index.html", [
+    [ "MAOS Documentation", "index.html", null ],
+    [ "Related Pages", "pages.html", [
+      [ "Introduction", "page1.html", null ],
+      [ "Misregistration", "page11.html", null ],
+      [ "Compile and Run the Code", "page2.html", null ],
+      [ "Simulation Parameters", "page3.html", null ],
+      [ "Simulation Results", "page4.html", null ],
+      [ "Geometry Data", "page41.html", null ],
+      [ "Telemetry Data", "page42.html", null ],
+      [ "Programming Guidelines", "page5.html", null ],
+      [ "Profiling MAOS", "page6.html", null ],
+      [ "C Fundementals", "page7.html", null ],
+      [ "Example: NFIRAOS", "nfiraos.html", null ],
+      [ "Example: SCAO NGS", "SCAO_NGS.html", null ],
+      [ "Example: SCAO LGS", "SCAO_LGS.html", null ],
+      [ "Example: SCAO BASE", "SCAO_BASE.html", null ],
+      [ "Todo List", "todo.html", null ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "_GtkCellRendererProgressnew", "struct__GtkCellRendererProgressnew.html", null ],
+      [ "_GtkCellRendererProgressnewClass", "struct__GtkCellRendererProgressnewClass.html", null ],
+      [ "APER_CFG_T", "structAPER__CFG__T.html", null ],
+      [ "APER_T", "structAPER__T.html", null ],
+      [ "ARG_S", "structARG__S.html", null ],
+      [ "ARG_T", "structARG__T.html", null ],
+      [ "ARGOPT_T", "structARGOPT__T.html", null ],
+      [ "ASTER_S", "structASTER__S.html", null ],
+      [ "ATM_CFG_T", "structATM__CFG__T.html", null ],
+      [ "ATMR_CFG_T", "structATMR__CFG__T.html", null ],
+      [ "ccell", "structccell.html", null ],
+      [ "cell", "structcell.html", null ],
+      [ "cellarr", "structcellarr.html", null ],
+      [ "cmat", "structcmat.html", null ],
+      [ "CN2EST_CFG_T", "structCN2EST__CFG__T.html", null ],
+      [ "CN2EST_T", "structCN2EST__T.html", null ],
+      [ "CN2PAIR_T", "structCN2PAIR__T.html", null ],
+      [ "csp", "structcsp.html", null ],
+      [ "cspcell", "structcspcell.html", null ],
+      [ "cuccell", "structcuccell.html", null ],
+      [ "cucmat", "structcucmat.html", null ],
+      [ "cudata_t", "structcudata__t.html", null ],
+      [ "cudtf_t", "structcudtf__t.html", null ],
+      [ "culoc_t", "structculoc__t.html", null ],
+      [ "cumap_t", "structcumap__t.html", null ],
+      [ "cumuv_t", "structcumuv__t.html", null ],
+      [ "curcell", "structcurcell.html", null ],
+      [ "curecon_t", "structcurecon__t.html", null ],
+      [ "curmat", "structcurmat.html", null ],
+      [ "cusp", "structcusp.html", null ],
+      [ "cuspcell", "structcuspcell.html", null ],
+      [ "cuwfs_t", "structcuwfs__t.html", null ],
+      [ "cuwloc_t", "structcuwloc__t.html", null ],
+      [ "DBG_CFG_T", "structDBG__CFG__T.html", null ],
+      [ "dcell", "structdcell.html", null ],
+      [ "DM_CFG_T", "structDM__CFG__T.html", null ],
+      [ "dmat", "structdmat.html", null ],
+      [ "drawdata_t", "structdrawdata__t.html", null ],
+      [ "dsp", "structdsp.html", null ],
+      [ "DTF_S", "structDTF__S.html", null ],
+      [ "DTF_T", "structDTF__T.html", null ],
+      [ "ETF_T", "structETF__T.html", null ],
+      [ "EVL_CFG_T", "structEVL__CFG__T.html", null ],
+      [ "FDPCG_T", "structFDPCG__T.html", null ],
+      [ "FIT_CFG_T", "structFIT__CFG__T.html", null ],
+      [ "FRACTAL_T", "structFRACTAL__T.html", null ],
+      [ "GENSCREEN_T", "structGENSCREEN__T.html", null ],
+      [ "GPU_CFG_T", "structGPU__CFG__T.html", null ],
+      [ "gpu_wfs_t", "structgpu__wfs__t.html", null ],
+      [ "HYST_T", "structHYST__T.html", null ],
+      [ "icell", "structicell.html", null ],
+      [ "imat", "structimat.html", null ],
+      [ "INTSTAT_T", "structINTSTAT__T.html", null ],
+      [ "INVPSD_T", "structINVPSD__T.html", null ],
+      [ "LLT_CFG_T", "structLLT__CFG__T.html", null ],
+      [ "LLT_T", "structLLT__T.html", null ],
+      [ "LOAD_CFG_T", "structLOAD__CFG__T.html", null ],
+      [ "loc_t", "structloc__t.html", null ],
+      [ "locmap_t", "structlocmap__t.html", null ],
+      [ "locstat_t", "structlocstat__t.html", null ],
+      [ "locstatcol_t", "structlocstatcol__t.html", null ],
+      [ "LSR_CFG_T", "structLSR__CFG__T.html", null ],
+      [ "MAOS_S", "structMAOS__S.html", null ],
+      [ "map_t", "structmap__t.html", null ],
+      [ "MOAO_CFG_T", "structMOAO__CFG__T.html", null ],
+      [ "MOAO_T", "structMOAO__T.html", null ],
+      [ "MONITOR_T", "structMONITOR__T.html", null ],
+      [ "mt_state", "structmt__state.html", null ],
+      [ "MUV_T", "structMUV__T.html", null ],
+      [ "NGSMOD_T", "structNGSMOD__T.html", null ],
+      [ "PARMS_S", "structPARMS__S.html", null ],
+      [ "PARMS_T", "structPARMS__T.html", null ],
+      [ "PISTAT_S", "structPISTAT__S.html", null ],
+      [ "PLOT_CFG_T", "structPLOT__CFG__T.html", null ],
+      [ "POWFS_CFG_T", "structPOWFS__CFG__T.html", null ],
+      [ "POWFS_S", "structPOWFS__S.html", null ],
+      [ "POWFS_T", "structPOWFS__T.html", null ],
+      [ "PROC_T", "structPROC__T.html", null ],
+      [ "PROPDATA_T", "structPROPDATA__T.html", null ],
+      [ "pts_t", "structpts__t.html", null ],
+      [ "QUEUE_T", "structQUEUE__T.html", null ],
+      [ "RECON_CFG_T", "structRECON__CFG__T.html", null ],
+      [ "RECON_T", "structRECON__T.html", null ],
+      [ "rectmap_t", "structrectmap__t.html", null ],
+      [ "RUN_T", "structRUN__T.html", null ],
+      [ "SAVE_CFG_T", "structSAVE__CFG__T.html", null ],
+      [ "scell", "structscell.html", null ],
+      [ "SERVO_T", "structSERVO__T.html", null ],
+      [ "SIM_CFG_T", "structSIM__CFG__T.html", null ],
+      [ "SIM_S", "structSIM__S.html", null ],
+      [ "SIM_SAVE_T", "structSIM__SAVE__T.html", null ],
+      [ "SIM_T", "structSIM__T.html", null ],
+      [ "SKYC_S", "structSKYC__S.html", null ],
+      [ "smat", "structsmat.html", null ],
+      [ "spcell", "structspcell.html", null ],
+      [ "ssp", "structssp.html", null ],
+      [ "sspcell", "structsspcell.html", null ],
+      [ "STAR_S", "structSTAR__S.html", null ],
+      [ "STATUS_T", "structSTATUS__T.html", null ],
+      [ "thread_t", "structthread__t.html", null ],
+      [ "TOMO_CFG_T", "structTOMO__CFG__T.html", null ],
+      [ "TYPEII_T", "structTYPEII__T.html", null ],
+      [ "vtbl", "structvtbl.html", null ],
+      [ "WFS_CFG_T", "structWFS__CFG__T.html", null ],
+      [ "WFS_S", "structWFS__S.html", null ],
+      [ "WFSINTS_T", "structWFSINTS__T.html", null ],
+      [ "zcell", "structzcell.html", null ],
+      [ "zmat", "structzmat.html", null ],
+      [ "zsp", "structzsp.html", null ],
+      [ "zspcell", "structzspcell.html", null ]
+    ] ],
+    [ "Data Structure Index", "classes.html", null ],
+    [ "Data Fields", "functions.html", null ],
+    [ "File List", "files.html", [
+      [ "lib/accphi.c", "accphi_8c.html", null ],
+      [ "lib/bin.c", "bin_8c.html", null ],
+      [ "lib/cellarr.c", "cellarr_8c.html", null ],
+      [ "lib/chol.c", "chol_8c.html", null ],
+      [ "lib/cmat.c", "cmat_8c.html", null ],
+      [ "lib/cmat.h", "cmat_8h.html", null ],
+      [ "lib/csp.h", "csp_8h.html", null ],
+      [ "lib/dmat.h", "dmat_8h.html", null ],
+      [ "lib/draw.c", "draw_8c.html", null ],
+      [ "lib/dsp.h", "dsp_8h.html", null ],
+      [ "lib/fft.c", "fft_8c.html", null ],
+      [ "lib/fractal.c", "fractal_8c.html", null ],
+      [ "lib/genotf.c", "genotf_8c.html", null ],
+      [ "lib/hashlittle.c", "hashlittle_8c.html", null ],
+      [ "lib/loc.c", "loc_8c.html", null ],
+      [ "lib/locbin.c", "locbin_8c.html", null ],
+      [ "lib/mathmisc.c", "mathmisc_8c.html", null ],
+      [ "lib/mkg.c", "mkg_8c.html", null ],
+      [ "lib/mkh.c", "mkh_8c.html", null ],
+      [ "lib/mkw.c", "mkw_8c.html", null ],
+      [ "lib/muv.c", "muv_8c.html", null ],
+      [ "lib/path.c", "path_8c.html", null ],
+      [ "lib/random.c", "random_8c.html", null ],
+      [ "lib/readcfg.h", "readcfg_8h.html", null ],
+      [ "lib/servo.c", "servo_8c.html", null ],
+      [ "lib/turbulence.c", "turbulence_8c.html", null ],
+      [ "lib/type.h", "type_8h.html", null ],
+      [ "lib/nr/bessik.c", "bessik_8c.html", null ],
+      [ "lib/sys/thread_pool.c", "thread__pool_8c.html", null ],
+      [ "maos/ahst.c", "ahst_8c.html", null ],
+      [ "maos/cachedm.c", "cachedm_8c.html", null ],
+      [ "maos/fdpcg.c", "fdpcg_8c.html", null ],
+      [ "maos/filter.c", "filter_8c.html", null ],
+      [ "maos/genseotf.c", "genseotf_8c.html", null ],
+      [ "maos/maos.c", "maos_8c.html", null ],
+      [ "maos/moao.c", "moao_8c.html", null ],
+      [ "maos/mtch.c", "maos_2mtch_8c.html", null ],
+      [ "maos/parms.h", "maos_2parms_8h.html", null ],
+      [ "maos/perfevl.c", "perfevl_8c.html", null ],
+      [ "maos/recon.c", "recon_8c.html", null ],
+      [ "maos/recon_utils.c", "recon__utils_8c.html", null ],
+      [ "maos/save.c", "save_8c.html", null ],
+      [ "maos/setup_aper.c", "setup__aper_8c.html", null ],
+      [ "maos/setup_parms.c", "maos_2setup__parms_8c.html", null ],
+      [ "maos/setup_powfs.c", "maos_2setup__powfs_8c.html", null ],
+      [ "maos/setup_recon.c", "setup__recon_8c.html", null ],
+      [ "maos/setup_surf.c", "setup__surf_8c.html", null ],
+      [ "maos/sim.c", "sim_8c.html", null ],
+      [ "maos/sim_utils.c", "sim__utils_8c.html", null ],
+      [ "maos/types.h", "maos_2types_8h.html", null ],
+      [ "maos/utils.c", "maos_2utils_8c.html", null ],
+      [ "maos/wfsgrad.c", "wfsgrad_8c.html", null ],
+      [ "maos/wfsints.c", "wfsints_8c.html", null ],
+      [ "skyc/genstars.c", "genstars_8c.html", null ],
+      [ "skyc/mtch.c", "skyc_2mtch_8c.html", null ],
+      [ "skyc/nafocus.c", "nafocus_8c.html", null ],
+      [ "skyc/parms.h", "skyc_2parms_8h.html", null ],
+      [ "skyc/photon.c", "photon_8c.html", null ],
+      [ "skyc/setup_aster.c", "setup__aster_8c.html", null ],
+      [ "skyc/setup_powfs.c", "skyc_2setup__powfs_8c.html", null ],
+      [ "skyc/setup_star.c", "setup__star_8c.html", null ],
+      [ "skyc/skyc.c", "skyc_8c.html", null ],
+      [ "skyc/skysim.c", "skysim_8c.html", null ],
+      [ "skyc/skysim_utils.c", "skysim__utils_8c.html", null ],
+      [ "skyc/types.h", "skyc_2types_8h.html", null ]
+    ] ],
+    [ "Globals", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
