4cb98a63bfb25c16933b397badbaabdcda481b8e
Lianqi Wang <lianqiw@gmail.com>
2011-10-10 06:04:09 +0000

    Fixing -fPIC

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,4 @@
-#aclocal --force -I m4 &&\
-#libtoolize --force --copy&&\
-#autoconf --force &&\
-#autoheader --force &&\
-#automake --add-missing --copy --force-missing
-#gtkdocize --copy
-#use glibtoolize in mac.
-
-#autoreconf --force --install -I m4
-#aclocal --force -I m4
+which libtoolize>/dev/null && libtoolize --copy || glibtoolize --copy
 aclocal --force -I m4
 autoheader --force
 automake --add-missing --copy --force-missing
