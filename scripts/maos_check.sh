f67eee0da38f8b9a6e0f83a27f97148b33893277
Lianqi Wang <lianqiw@gmail.com>
2014-04-02 10:52:01 -0700

    Fixed TOMOSCALE for LSR.

--- scripts/maos_check.sh
+++ scripts/maos_check.sh
@@ -13,6 +13,15 @@ echo "D is ${D}m. DM order is $((D*2))."
 echo -n "LGS MCAO: "
 echo $(./maos aper.d=$D dm.order=[$D*2 $D*2]  2>>maos_check.stderr) nm
 
+echo -n "LGS MCAO (FDPCG): "
+echo $(./maos aper.d=$D dm.order=[$D*2 $D*2] tomo.precond=1 2>>maos_check.stderr) nm
+
+echo -n "LGS MCAO (CBS):  "
+echo $(./maos aper.d=$D dm.order=[$D*2 $D*2] tomo.alg=0 fit.alg=0 2>>maos_check.stderr) nm
+
+echo -n "LGS MCAO (SVD):  "
+echo $(./maos aper.d=$D dm.order=[$D*2 $D*2] tomo.alg=2 fit.alg=2 2>>maos_check.stderr) nm
+
 echo -n "LGS MCAO $((D*4))x$((D*4)):  "
 echo $(./maos aper.d=$D dm.order=[$D*4 $D*4] 2>>maos_check.stderr ) nm
 
@@ -22,14 +31,23 @@ echo $(./maos aper.d=$D dm.order=[$D*2 $D*2] evl.moao=0 moao.order=[$D] 2>>maos_
 echo -n "LGS GLAO:  "
 echo $(./maos aper.d=$D dm_single.conf dm.order=[$D*2] recon.glao=1 wfs_lgs_only.conf 2>>maos_check.stderr ) nm
 
-echo -n "NGS SCAO:      "
+echo -n "NGS SCAO:         "
 echo $(./maos aper.d=$D dm.order=[$D*2] -cscao_ngs.conf 2>>maos_check.stderr ) nm
+
+echo -n "NGS SCAO (split): "
+echo $(./maos aper.d=$D dm.order=[$D*2] -cscao_ngs.conf recon.split=1 2>>maos_check.stderr ) nm
 
 echo -n "NGS MCAO: "
 echo $(./maos aper.d=$D dm.order=[$D*2 $D*2] -cmcao_ngs.conf 2>>maos_check.stderr ) nm
 
+echo -n "NGS MCAO (split): "
+echo $(./maos aper.d=$D dm.order=[$D*2 $D*2] -cmcao_ngs.conf recon.split=1 2>>maos_check.stderr ) nm
+
 echo -n "SCAO LGS: "
 echo $(./maos aper.d=$D dm.order=[$D*2] -cscao_lgs.conf 2>>maos_check.stderr ) nm
+
+echo -n "SCAO LGS (inte): "
+echo $(./maos aper.d=$D dm.order=[$D*2] -cscao_lgs.conf recon.split=0 2>>maos_check.stderr ) nm
 
 echo -n "MVM mode:       "
 echo $(./maos aper.d=$D dm.order=[$D*2 $D*2] atmr.os=[2 2 2 2 2 2] tomo.precond=1 tomo.maxit=100 fit.alg=0 2>>maos_check.stderr) nm
