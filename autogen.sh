2dc0981403962413615bb6dbb62a4f3e4ecd2951
Lianqi Wang <lianqiw@gmail.com>
2014-06-25 23:26:47 -0400

    Enables compiling dynamic libraries in cuda.

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,6 @@ which libtoolize>/dev/null && libtoolize --copy --force || glibtoolize --copy --
 aclocal --force -I m4
 autoheader --force
 automake --add-missing --copy --force-missing
+sed 's/-fno-common//g' m4/libtool.m4 > m4/libtool_new.m4
+mv m4/libtool_new.m4 m4/libtool.m4
 autoconf --force
