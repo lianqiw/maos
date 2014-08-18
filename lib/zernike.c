ff8aa44d9f0c3e1b3adeffb2a76b64a832764def
Lianqi Wang <lianqiw@gmail.com>
2014-08-18 15:06:50 -0700

    Limit maximum mode of zernike modes to number of points

--- lib/zernike.c
+++ lib/zernike.c
@@ -54,13 +54,18 @@ static dmat *genRnm(const dmat *locr, int ir, int im){
    
 */
 dmat* zernike(const loc_t *loc, double D, int nr){
+    int nr3=(int)floor((sqrt(loc->nloc*8+1)-3)*0.5);
+    if(nr>nr3){
+	warning("Reduce nr=%d to %d\n", nr, nr3);	
+	nr=nr3;
+    }
     double D2=loc_diam(loc);
     if(D<=0){
 	D=D2;
     }else if(fabs(D-D2)>loc->dx*10){
-	error("specified diameter is incorrect\n");
+	error("specified diameter is incorrect. D=%g, loc D=%g\n", D, D2);
     }else if(fabs(D-D2)>loc->dx*2){
-	warning("specified diameter is incorrect\n");
+	warning("specified diameter is incorrect. D=%g, loc D=%g\n", D, D2);
     }
     if(nr<0) error("Invalid nr\n");
     int nmod=(nr+1)*(nr+2)/2;
@@ -223,10 +228,19 @@ dmat *diag_mod_cov(const dmat *mz, /**<Modes in zernike space*/
    better accuracy. nr2 controls this overshoot.
  */
 dmat *KL_kolmogorov(const loc_t *loc, double D, int nr, int nr2){
+    int nr3=(int)floor((sqrt(loc->nloc*8+1)-3)*0.5);
     if(nr2<=0){
 	nr2=nr*2;
     }else if(nr2<nr){
 	error("nr2=%d cannot be smaller than nr=%d\n", nr2, nr);
+    }
+    if(nr2>nr3){
+	warning("Reduce nr2=%d to %d\n", nr2, nr3);
+	nr2=nr3;
+    }
+    if(nr>nr3){
+	warning("Reduce nr=%d to %d\n", nr, nr3);	
+	nr=nr3;
     }
     dmat *cov=zernike_cov_kolmogorov(nr2);
     dmat *modz=zernike(loc, D, nr2);
