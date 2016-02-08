8215c7ecff44e0c2c674db716342473d3210edbe
Lianqi Wang <lianqiw@gmail.com>
2016-02-08 10:25:12 -0800

    Happy new year!

--- scripts/addcopy.sh
+++ scripts/addcopy.sh
@@ -2,15 +2,18 @@ files=$(ls ../maos/*.[ch] ../sys/*.[ch] ../lib/*.[ch] ../skyc/*.[ch] ../tools/*.
 
 for file in $files;do
     if grep -q Copyright $file;then
-	echo Stripping old Copyright from $file
 	line=$(grep -m 1 -n www.gnu.org $file |cut -d ':' -f 1)
 	echo line=$line
-	line=$((line+1)) || continue
-	sed -i "1,${line}d" $file || continue
+	line=$((line+1)) 
+	if [ $line -ne 17 ];then
+	    echo skip $file: wrong format
+	else
+	    sed -i "1,${line}d" $file || continue
+	fi
     fi
 
     if ! grep -q Copyright $file; then
-	echo will add to $file
+	echo Updated copyright information from $file
 	cat copy >>$file.new 
 	echo     >>$file.new
 	cat $file>>$file.new
--- sys/process_linux.c
+++ sys/process_linux.c
@@ -1,5 +1,5 @@
 /*
-  Copyright 2009-2015 Lianqi Wang <lianqiw@gmail.com> <lianqiw@tmt.org>
+  Copyright 2009-2016 Lianqi Wang <lianqiw-at-tmt-dot-org>
   
   This file is part of Multithreaded Adaptive Optics Simulator (MAOS).
 
