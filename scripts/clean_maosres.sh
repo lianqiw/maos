9fc7c63a57ac9ddd9ac17056d25fb252d394c794
Lianqi Wang <lianqiw@gmail.com>
2017-08-25 14:13:31 -0700

    A script to clean extra files in maos folder.

---
+++ scripts/clean_maosres.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#Clean maos results folder, leaving only Res_${seed}.bin, Res_${seed}.done, *.conf, *.log
+
+for fd in "$@" ;do
+    rm -rf ${fd}/setup ${fd}/Res[a-zA-Z]*.bin ${fd}/Timing_*.bin ${fd}/psd*.bin
+done
+    
