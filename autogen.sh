cfb2ce210689949b2d240f4621fd03cf7ffb73cf
Lianqi Wang <lianqiw@gmail.com>
2019-11-13 10:30:34 -0800

    Use autoreconf in autogen.sh as the most portable way.

--- autogen.sh
+++ autogen.sh
@@ -1,21 +1,18 @@
-#autoscan: scans source file to configure.scan as a template for configure.ac
-#aclocal: grenate aclocal.m4 by scanning configure.ac
-#autoheader: scan configure.ac to create a header template file config.h.in for config.h
-#libtoolize: wraps system libtool to a standard libtool script.
-#automake: Creates Makefile.in from Makefile.am
-#autoconf: Generates configure from configure.ac
-if which libtoolize >/dev/null 2>&1 ; then
-    libtoolize --copy --force
-else
-    glibtoolize --copy --force
-fi
-sed 's/-fno-common//g' m4/libtool.m4 > m4/libtool_new.m4
-mv m4/libtool_new.m4 m4/libtool.m4
-aclocal --install -I m4
-autoheader -f
-autoconf 
-automake --add-missing --copy --foreign
+#Autotools includes two Core Packages: GNU Autoconf and GNU Automake
+#GNU Autoconf include the following tools
+# autoconf:   Creates configure from configure.ac
+# autoheader: Creates config.h.in from configure.ac
+# autoreconf: Run all tools in the right order
+# autoscan:   Scan sources for common portability problems and related macros missing from configure.ac
+# autoupdate: Update obsolete macros in configure.ac
+# ifnames:    Gather identifiers from all #if/#ifdef/... directives
+# autom4te:   The heart of Autoconf. It drives M4 and implements the features used by most of the above tools.
 
+#GNU Automake includes the following tools
+# automake:   Create Makefile.in from Makefile.am and configure.ac
+# aclocal:    Scan configure.ac for uses of third-party macros and gather definitions in aclocal.m4
 
-#To compile in 32 bit mode in 64 bit machines, run configure.ac with the following options
-# --host=i686-pc-linux-gnu CFLAGS="-m32" LDFLAGS="-m32" CXXFLAGS="-m32"
+#GNU M4 is the real macro processor. 
+
+#Use autoreconf to setup the package initially
+autoreconf -i
