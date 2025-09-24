81ed15e9d134c46b7feb8dd735dfd16bad4ac822
Lianqi <lianqiw@gmail.com>
2025-09-24 22:15:03 +0000

    Automatically updated

--- search/all_6.js
+++ search/all_6.js
@@ -206,7 +206,7 @@ var searchData=
   ['cnorm_203',['cnorm',['../de/d07/cmath_8h.html#aef8ed03215c7d2d139b4e27601225e41',1,'ctmp.c']]],
   ['cnormalize_5fsum_204',['cnormalize_sum',['../de/d07/cmath_8h.html#a27798d8582ca81cce96872b58fdc03d0',1,'ctmp.c']]],
   ['cnormalize_5fsumabs_205',['cnormalize_sumabs',['../de/d07/cmath_8h.html#af58a2e752d27e465c1a5e6cd14766d5b',1,'ctmp.c']]],
-  ['code_206',['code',['../d3/d4c/page20_compile.html',1,'Compile the Code'],['../d3/d4c/page20_compile.html#autotoc_md7',1,'Compile the Code'],['../d3/d4c/page20_compile.html#autotoc_md2',1,'Download the code']]],
+  ['code_206',['code',['../d3/d4c/page20_compile.html#autotoc_md7',1,'Compile the Code'],['../d3/d4c/page20_compile.html',1,'Compile the Code'],['../d3/d4c/page20_compile.html#autotoc_md2',1,'Download the code']]],
   ['cogoff_207',['cogoff',['../d4/d38/parms_8h.html#a6baba8c243f71803bcb842733673269d',1,'powfs_cfg_t']]],
   ['cogthres_208',['cogthres',['../d4/d38/parms_8h.html#a309dc28c7e1c90cb414d20bb5fdf9f67',1,'powfs_cfg_t']]],
   ['coldtrat_209',['coldtrat',['../d4/d38/parms_8h.html#a708807d801ef0c550506f132f56d3377',1,'llt_cfg_t']]],
