fe330bca17a497d79a52750b14c3121600517621
Lianqi Wang <lianqiw@gmail.com>
2026-01-01 15:09:19 -0800

    Happy 2026

--- lib/comb.c
+++ lib/comb.c
@@ -1,5 +1,5 @@
 /*
-  Copyright 2009-2025 Lianqi Wang <lianqiw-at-tmt-dot-org>
+  Copyright 2009-2026 Lianqi Wang
   
   This file is part of Multithreaded Adaptive Optics Simulator (MAOS).
 
@@ -15,7 +15,6 @@
   You should have received a copy of the GNU General Public License along with
   MAOS.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #include "comb.h"
 /**
    Computes the number of possibilities of selection k items from n items \f$C_n^k\f$:
